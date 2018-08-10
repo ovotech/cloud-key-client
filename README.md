07e9d96fd1d316aa1cd91bfa13fecda7c030466b
eversC <chris.every@ovoenergy.com>
2018-08-10 19:14:37 +0100

    Add keys.go

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# key-collector
+# Cloud-Key-Client
+
+Cloud-Key-Client is a Golang client that connects up to cloud providers either to collect details of
+Service Account keys, or manipulate them.
+
+Amongst the details collected is the age (in minutes) of each key. This could
+prove useful for applications that apply further analysis and/or processing on
+them, such as key rotation.
+
+The following providers have been integrated:
+
+* AWS
+* GCP
+
+No config is required, you simply need to pass a slice of `providerRequest`
+structs to the `keys()` func.
+
+Authorisation is handled by the Default Credential Provider Chains for both
+[GCP](https://cloud.google.com/docs/authentication/production#auth-cloud-implicit-go) and [AWS](https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/credentials.html#credentials-default).
---
+++ keys.go
@@ -0,0 +1,177 @@
+package keys
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	awsiam "github.com/aws/aws-sdk-go/service/iam"
+	"golang.org/x/oauth2/google"
+	gcpiam "google.golang.org/api/iam/v1"
+)
+
+//type key
+type key struct {
+	age           float64
+	name          string
+	id            string
+	provider      string
+	lifeRemaining float64
+}
+
+type providerRequest struct {
+	provider   string
+	gcpProject string
+}
+
+const (
+	gcpTimeFormat           = "2006-01-02T15:04:05Z"
+	gcpServiceAccountPrefix = "serviceAccounts/"
+	gcpServiceAccountSuffix = "@"
+	gcpKeyPrefix            = "keys/"
+	gcpKeySuffix            = ""
+	gcpProviderString       = "gcp"
+	awsProviderString       = "aws"
+)
+
+//keys returns a generic key slice of potentially multiple provider keys
+func keys(providers []providerRequest) (keys []key) {
+	for _, providerRequest := range providers {
+		switch providerRequest.provider {
+		case "gcp":
+			keys = appendSlice(keys, gcpKeys(providerRequest.gcpProject))
+		case "aws":
+			keys = appendSlice(keys, awsKeys())
+		default:
+			panic("No valid providers specified. Must be gcp|aws")
+		}
+	}
+	return
+}
+
+//gcpKeys returns a slice of generic keys with provider=gcp
+func gcpKeys(gcpProject string) (keys []key) {
+	ctx := context.Background()
+	client, err := google.DefaultClient(ctx, gcpiam.CloudPlatformScope)
+	check(err)
+	service, err := gcpiam.New(client)
+	check(err)
+	for _, acc := range gcpServiceAccounts(gcpProject, *service) {
+		for _, gcpKey := range gcpServiceAccountKeys(gcpProject, acc.Email, *service) {
+			//only iterate over keys that have a mins-to-expiry, or are of an age,
+			// above a specific threshold, to differentiate between GCP-managed
+			// and User-managed keys:
+			// https://cloud.google.com/iam/docs/understanding-service-accounts
+			keyAge := minsSince(parseTime(gcpTimeFormat, gcpKey.ValidAfterTime))
+			keyMinsToExpiry := minsSince(parseTime(gcpTimeFormat,
+				gcpKey.ValidBeforeTime))
+			keys = append(keys, key{keyAge,
+				subString(gcpKey.Name, gcpServiceAccountPrefix,
+					gcpServiceAccountSuffix),
+				subString(gcpKey.Name, gcpKeyPrefix, gcpKeySuffix),
+				gcpProviderString, keyMinsToExpiry})
+		}
+	}
+	return
+}
+
+//gcpServiceAccounts returns a slice of GCP ServiceAccounts
+func gcpServiceAccounts(project string, service gcpiam.Service) (accs []*gcpiam.ServiceAccount) {
+	res, err := service.Projects.ServiceAccounts.List(fmt.Sprintf("projects/%s",
+		project)).
+		Do()
+	check(err)
+	accs = res.Accounts
+	return
+}
+
+//gcpServiceAccountKeys returns a slice of ServiceAccountKeys
+func gcpServiceAccountKeys(project, email string, service gcpiam.Service) (keys []*gcpiam.ServiceAccountKey) {
+	res, err := service.Projects.ServiceAccounts.Keys.
+		List(fmt.Sprintf("projects/%s/serviceAccounts/%s", project, email)).
+		Do()
+	check(err)
+	keys = res.Keys
+	return
+}
+
+//awskeys returns a slice of generic keys with provider=aws
+func awsKeys() (keys []key) {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1")},
+	)
+	check(err)
+	svc := awsiam.New(sess)
+	userResult, err := svc.ListUsers(&awsiam.ListUsersInput{
+		MaxItems: aws.Int64(10),
+	})
+	check(err)
+	for _, user := range userResult.Users {
+		fmt.Println(*user.UserId)
+		result, err := svc.ListAccessKeys(&awsiam.ListAccessKeysInput{
+			MaxItems: aws.Int64(5),
+			UserName: aws.String(*user.UserName),
+		})
+		check(err)
+		for _, awsKey := range result.AccessKeyMetadata {
+			keys = append(keys,
+				key{minsSince(*awsKey.CreateDate),
+					*awsKey.UserName, *awsKey.AccessKeyId, awsProviderString, 0})
+		}
+	}
+	return
+}
+
+//appendSlice appends the 2nd slice to the 1st, and returns the resulting slice
+func appendSlice(keys, keysToAdd []key) []key {
+	for _, keyToAdd := range keysToAdd {
+		keys = append(keys, keyToAdd)
+	}
+	return keys
+}
+
+//check panics if error is not nil
+func check(e error) {
+	if e != nil {
+		panic(e.Error())
+	}
+}
+
+//parseTime calls time.Parse with the timeFormat and timeString provided, and
+// checks for an error
+func parseTime(timeFormat, timeString string) (then time.Time) {
+	then, err := time.Parse(timeFormat, timeString)
+	check(err)
+	return
+}
+
+//minsSinceCreation returns the number of minutes since the provided time.Time
+func minsSince(then time.Time) (minsSinceCreation float64) {
+	duration := time.Since(then)
+	minsSinceCreation = duration.Minutes()
+	return
+}
+
+//substring returns a non-inclusive substring between the provided start and
+// end strings. If neither start or end strings exist, it panics. Specify empty
+// string as the 'end' parameter to use the length of str as the end index
+func subString(str string, start string, end string) (result string) {
+	startIndex := strings.Index(str, start)
+	if startIndex != -1 {
+		startIndex += len(start)
+		endIndex := len(str)
+		if len(end) > 0 {
+			endIndex = strings.Index(str, end)
+			if endIndex == -1 {
+				panic("string " + end + "not found in target: " + str)
+			}
+		}
+		result = str[startIndex:endIndex]
+	} else {
+		panic("string " + start + "not found in target: " + str)
+	}
+	return
+}
