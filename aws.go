a2908a64b784780f88b925bcb7688f33cf9331cc
eversC <chris.every@ovoenergy.com>
2018-10-19 10:51:51 +0100

    Split AWS and GCP funcs into their own files

---
+++ aws.go
@@ -0,0 +1,39 @@
+package keys
+
+import (
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+
+	awsiam "github.com/aws/aws-sdk-go/service/iam"
+)
+
+//awskeys returns a slice of generic keys with provider=aws
+func awsKeys() (keys []Key) {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1")},
+	)
+	check(err)
+	svc := awsiam.New(sess)
+	userResult, err := svc.ListUsers(&awsiam.ListUsersInput{
+		MaxItems: aws.Int64(10),
+	})
+	check(err)
+	for _, user := range userResult.Users {
+		result, err := svc.ListAccessKeys(&awsiam.ListAccessKeysInput{
+			MaxItems: aws.Int64(5),
+			UserName: aws.String(*user.UserName),
+		})
+		check(err)
+		for _, awsKey := range result.AccessKeyMetadata {
+			keyID := *awsKey.AccessKeyId
+			keys = append(keys,
+				Key{minsSince(*awsKey.CreateDate),
+					strings.Join([]string{*awsKey.UserName,
+						keyID[len(keyID)-numIDValuesInName:]}, "_"),
+					keyID, awsProviderString, 0})
+		}
+	}
+	return
+}
--- keys.go
+++ keys.go
@@ -1,17 +1,8 @@
 package keys
 
 import (
-	"context"
-	"fmt"
-	"math"
 	"strings"
 	"time"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	awsiam "github.com/aws/aws-sdk-go/service/iam"
-	"golang.org/x/oauth2/google"
-	gcpiam "google.golang.org/api/iam/v1"
 )
 
 //Key type
@@ -50,82 +41,6 @@ func Keys(providers []Provider) (keys []Key) {
 			keys = appendSlice(keys, awsKeys())
 		default:
 			panic("No valid providers specified. Must be gcp|aws")
-		}
-	}
-	return
-}
-
-//gcpKeys returns a slice of generic keys with provider=gcp
-func gcpKeys(gcpProject string) (keys []Key) {
-	ctx := context.Background()
-	client, err := google.DefaultClient(ctx, gcpiam.CloudPlatformScope)
-	check(err)
-	service, err := gcpiam.New(client)
-	check(err)
-	for _, acc := range gcpServiceAccounts(gcpProject, *service) {
-		for _, gcpKey := range gcpServiceAccountKeys(gcpProject, acc.Email,
-			*service) {
-			keyAge := minsSince(parseTime(gcpTimeFormat, gcpKey.ValidAfterTime))
-			keyID := subString(gcpKey.Name, gcpKeyPrefix, gcpKeySuffix)
-			keyMinsToExpiry := math.Abs(minsSince(parseTime(gcpTimeFormat,
-				gcpKey.ValidBeforeTime)))
-			serviceAccountName := subString(gcpKey.Name, gcpServiceAccountPrefix,
-				gcpServiceAccountSuffix)
-			keys = append(keys, Key{keyAge,
-				strings.Join([]string{serviceAccountName,
-					keyID[len(keyID)-numIDValuesInName:]}, "_"),
-				keyID,
-				gcpProviderString, keyMinsToExpiry})
-		}
-	}
-	return
-}
-
-//gcpServiceAccounts returns a slice of GCP ServiceAccounts
-func gcpServiceAccounts(project string, service gcpiam.Service) (accs []*gcpiam.ServiceAccount) {
-	res, err := service.Projects.ServiceAccounts.List(fmt.Sprintf("projects/%s",
-		project)).
-		Do()
-	check(err)
-	accs = res.Accounts
-	return
-}
-
-//gcpServiceAccountKeys returns a slice of ServiceAccountKeys
-func gcpServiceAccountKeys(project, email string, service gcpiam.Service) (keys []*gcpiam.ServiceAccountKey) {
-	res, err := service.Projects.ServiceAccounts.Keys.
-		List(fmt.Sprintf("projects/%s/serviceAccounts/%s", project, email)).
-		KeyTypes("USER_MANAGED").
-		Do()
-	check(err)
-	keys = res.Keys
-	return
-}
-
-//awskeys returns a slice of generic keys with provider=aws
-func awsKeys() (keys []Key) {
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String("us-east-1")},
-	)
-	check(err)
-	svc := awsiam.New(sess)
-	userResult, err := svc.ListUsers(&awsiam.ListUsersInput{
-		MaxItems: aws.Int64(10),
-	})
-	check(err)
-	for _, user := range userResult.Users {
-		result, err := svc.ListAccessKeys(&awsiam.ListAccessKeysInput{
-			MaxItems: aws.Int64(5),
-			UserName: aws.String(*user.UserName),
-		})
-		check(err)
-		for _, awsKey := range result.AccessKeyMetadata {
-			keyID := *awsKey.AccessKeyId
-			keys = append(keys,
-				Key{minsSince(*awsKey.CreateDate),
-					strings.Join([]string{*awsKey.UserName,
-						keyID[len(keyID)-numIDValuesInName:]}, "_"),
-					keyID, awsProviderString, 0})
 		}
 	}
 	return
