d1cf19b62780ca917769b8dab444b6626808062b
eversC <chris.every@ovoenergy.com>
2019-02-12 17:56:06 +0000

    AWS: add createKey functionality

--- aws.go
+++ aws.go
@@ -1,6 +1,7 @@
 package keys
 
 import (
+	"strconv"
 	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -9,24 +10,14 @@ import (
 	awsiam "github.com/aws/aws-sdk-go/service/iam"
 )
 
+const accessKeyLimit = 2
+
 //awskeys returns a slice of generic keys with provider=aws
 func awsKeys() (keys []Key) {
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String("us-east-1")},
-	)
-	check(err)
-	svc := awsiam.New(sess)
-	userResult, err := svc.ListUsers(&awsiam.ListUsersInput{
-		MaxItems: aws.Int64(10),
-	})
-	check(err)
-	for _, user := range userResult.Users {
-		result, err := svc.ListAccessKeys(&awsiam.ListAccessKeysInput{
-			MaxItems: aws.Int64(5),
-			UserName: aws.String(*user.UserName),
-		})
-		check(err)
-		for _, awsKey := range result.AccessKeyMetadata {
+	// API ref: https://docs.aws.amazon.com/sdk-for-go/api/service/iam/
+	svc := awsiam.New(awsSession())
+	for _, user := range awsUserList(*svc) {
+		for _, awsKey := range awsKeyList(*user.UserName, *svc) {
 			keyID := *awsKey.AccessKeyId
 			keys = append(keys, Key{
 				*awsKey.UserName,
@@ -40,5 +31,50 @@ func awsKeys() (keys []Key) {
 			})
 		}
 	}
+	return
+}
+
+func awsSession() (sess *session.Session) {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1")},
+	)
+	check(err)
+	return
+}
+
+func awsUserList(iamService awsiam.IAM) (users []*awsiam.User) {
+	userResult, err := iamService.ListUsers(&awsiam.ListUsersInput{
+		MaxItems: aws.Int64(10),
+	})
+	check(err)
+	users = userResult.Users
+	return
+}
+
+func awsKeyList(username string, iamService awsiam.IAM) (accessKeyMetadata []*awsiam.AccessKeyMetadata) {
+	result, err := iamService.ListAccessKeys(&awsiam.ListAccessKeysInput{
+		MaxItems: aws.Int64(5),
+		UserName: aws.String(username),
+	})
+	check(err)
+	accessKeyMetadata = result.AccessKeyMetadata
+	return
+}
+
+func awsCreateKey(username string) (accessKeyID, secretAccessKey string) {
+	//get number of keys that currently exist
+	svc := awsiam.New(awsSession())
+	if len(awsKeyList(username, *svc)) >= accessKeyLimit {
+		panic("Number of Access Keys for user: " + username + "is already at its limit (" +
+			strconv.Itoa(accessKeyLimit) +
+			")")
+	}
+	key, err := svc.CreateAccessKey(&awsiam.CreateAccessKeyInput{
+		UserName: aws.String(username),
+	})
+	check(err)
+	accessKey := key.AccessKey
+	accessKeyID = *accessKey.AccessKeyId
+	secretAccessKey = *accessKey.SecretAccessKey
 	return
 }
